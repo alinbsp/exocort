0c36f57af532126aeceb27f5edfe7264c201a2c6
Alin Iacob <alin@thegeek.org>
2014-05-14 12:51:58 +0300

    handle static rendering

--- src/exocort/handler.clj
+++ src/exocort/handler.clj
@@ -6,11 +6,18 @@
 
 (def index (enlive/html-resource "public/hello.html"))
 
+(defn st-render
+  [static]
+  (enlive/emit*
+   (enlive/html-resource
+    (clojure.string/join "" ["public/" static ".html"]))))
+
 
 (defroutes app-routes
-  (GET "/" [] (enlive/emit* index))
+  (GET "/" [] (st-render "hello"))
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
   (handler/site app-routes))
+
--- src/exocort/templates.clj
+++
@@ -1,6 +0,0 @@
-(ns exocort.templates
-  (:require [net.cgrand.enlive-html :refer [deftemplate content]]))
-
-(deftemplate tpl-hello "public/hello.html"
-  [value]
-  [:#message] (content value))
